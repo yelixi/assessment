692eaaacb7c83d20dfe2cee7cd565fcffb33bfb2
B19040229谭永锋 <tan13621251388@gmail.com>
2021-08-07 16:35:48 +0800

    添加各个用户的菜单栏

--- src/main/java/com/sacc/assessment/controller/IssuerController.java
+++ src/main/java/com/sacc/assessment/controller/IssuerController.java
@@ -7,10 +7,7 @@ import com.sacc.assessment.service.ExamPaperService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -21,10 +18,16 @@ import java.util.List;
  */
 @PreAuthorize("hasRole('ISSUER')")
 @Controller
+@RequestMapping("/issuer")
 public class IssuerController {
 
     @Resource
     private ExamPaperService examPaperService;
+
+    @GetMapping("/")
+    public String home(){
+        return "/issuer/index";
+    }
 
     @ResponseBody
     @PostMapping("/createExam")
--- src/main/java/com/sacc/assessment/controller/UserController.java
+++ src/main/java/com/sacc/assessment/controller/UserController.java
@@ -2,13 +2,20 @@ package com.sacc.assessment.controller;
 
 import com.sacc.assessment.entity.User;
 import com.sacc.assessment.model.RestResult;
+import com.sacc.assessment.model.UserDetail;
 import com.sacc.assessment.service.UserService;
+import org.bouncycastle.math.raw.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
 
 /**
  * Created by 林夕
@@ -37,7 +44,11 @@ public class UserController {
     }
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model, Principal principal){
+        Object details = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetail user = (UserDetail) details;
+//        System.out.println(user.getRole());
+        model.addAttribute("ROLE",user.getRole());
         return "../static/html/home.html";
     }
 }
