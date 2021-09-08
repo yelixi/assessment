f17d45af3d6169ed9d934ab65cb2511130538e65
linxi <2790673868@qq.com>
2021-09-08 10:54:55 +0800

    完成了exam页面的md适配以及选择题适配

---
+++ src/main/java/com/sacc/assessment/conf/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.sacc.assessment.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by 林夕
+ * Date 2021/8/5 12:02
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路由
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+}
